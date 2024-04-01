5bbbd2a17764eceaac439d5b71f122399b07754c
Akshit Kumar Chandora <axitchandora@gmail.com>
2024-04-01 23:37:49 +0530

    Added initializr generated spring boot project

---
+++ src/main/java/com/axitchandora/codehack/CodehackApplication.java
@@ -0,0 +1,13 @@
+package com.axitchandora.codehack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodehackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodehackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axitchandora/codehack/CodehackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axitchandora.codehack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodehackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
